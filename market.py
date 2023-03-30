1ed846f4daf8f4e65ba6bb8a2dfa8e3fa9aaff0e
glenchiridza <glenchiridz@gmail.com>
2023-03-30 21:45:46 +0200

    market route:

--- market.py
+++ market.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return "hello glen"
+def home_page():
+    return render_template("home.html")
 
-if __name__ == "__main__":
-    app.run(debug=True,host='127.0.0.1',port=8080)
+
+@app.route('/market')
+def market_page():
+    return render_template('market.html',item_name='phone')
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True,host='127.0.0.1',port=8080)
