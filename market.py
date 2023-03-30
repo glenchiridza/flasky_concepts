ce5640185d299d7c4eeecbef0a7c2dab99b528b7
glenchiridza <glenchiridz@gmail.com>
2023-03-30 21:51:08 +0200

    render jinja

--- market.py
+++ market.py
@@ -8,9 +8,15 @@ def home_page():
     return render_template("home.html")
 
 
-@app.route('/market')
+@app.route('/market/')
 def market_page():
-    return render_template('market.html',item_name='phone')
+    items = [
+        {'id':1,'name':'phone','barcode':'2342234242','price':600},
+        {'id':2,'name':'tablet','barcode':'236366f433','price':700},
+        {'id':3,'name':'laptop','barcode':'0987383ghbb','price':1800},
+        {'id':4,'name':'keyboard','barcode':'32gf6674fg','price':800},
+    ]
+    return render_template('market.html',items=items)
 
 
 # if __name__ == "__main__":
