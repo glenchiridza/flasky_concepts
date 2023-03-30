a69a4835fed5bb2c814b82af7aeba5b177a9ce95
glenchiridza <glenchiridz@gmail.com>
2023-03-30 22:44:30 +0200

    Add to package

--- run.py
+++ run.py
@@ -1,8 +1,4 @@
-from flask import Flask,render_template
+from market import app
 
-from flask_sqlalchemy import SQLAlchemy
-
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///market.db"
-db = SQLAlchemy(app)
+if __name__ == "__main__":
+    app.run(debug=True)
