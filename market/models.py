db163d06b508c49b283ed01fa926f6ea1ffff09a
glenchiridza <glenchiridz@gmail.com>
2023-03-31 13:40:46 +0200

    relationships

--- market/models.py
+++ market/models.py
@@ -1,12 +1,28 @@
 from market import db
 
 
+class User(db.Model):
+    id = db.Column(db.Integer(),primary_key=True)
+    username = db.Column(db.String(length=30),nullable=False,unique=True)
+    email = db.Column(db.String(length=50),nullable=False,unique=True)
+    password_hash = db.Column(db.String(length=60),nullable=False)
+    budget = db.Column(db.Integer(),default=1000)
+    items = db.relationship('Item',backref='owning_user',lazy=True)
+
+    def __repr__(self):
+        return self.username
+
+
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=30), nullable=False, unique=True)
     description = db.Column(db.String(length=1024), nullable=False, unique=True)
+    owner = db.Column(db.Integer(),db.ForeignKey('user.id'))
 
     def __repr__(self):
         return self.name
+
+
+# ended at 2:10
