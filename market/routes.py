c20283c2302a6483f4abfd92107607cd1588a83f
glenchiridza <glenchiridz@gmail.com>
2023-03-31 15:06:46 +0200

    login start

--- market/routes.py
+++ market/routes.py
@@ -33,3 +33,9 @@ def register_page():
         for err_msg in form.errors.values():
             flash(f'There was an error creating a user: {err_msg}',category='danger')
     return render_template('register.html', form=form)
+
+
+@app.route('/login',methods=['GET','POST'])
+def login_page():
+
+    return render_template('login.html',form=form)
