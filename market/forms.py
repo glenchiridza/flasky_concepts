18aa76f97f3dc7473f0220a16140117d7af236fe
glenchiridza <glenchiridz@gmail.com>
2023-03-31 14:15:14 +0200

    validaors

--- market/forms.py
+++ market/forms.py
@@ -1,10 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Email, EqualTo, Length
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='username')
-    email_address = StringField(label='email')
-    password1 = PasswordField(label='password')
-    password2 = PasswordField(label='confirm password')
+    username = StringField(label='username', validators=Length(min=2, max=30))
+    email_address = StringField(label='email', validators=Email())
+    password1 = PasswordField(label='password',
+                              validators=Length(min=6))
+    password2 = PasswordField(label='confirm password', validators=EqualTo('password1'))
     submit = SubmitField(label='submit')
--- market/routes.py
+++ market/routes.py
@@ -1,5 +1,9 @@
-from market import app,render_template
-from market.models import Item
+from flask import render_template, redirect, url_for
+from market import app
+from market.models import Item, User
+from market.forms import RegisterForm
+
+from market import db
 
 
 @app.route('/')
@@ -11,3 +15,18 @@ def home_page():
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
+
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(
+            username=form.username.data,
+            email=form.email_address.data,
+            password_hash=form.password1.data
+        )
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    return render_template('register.html', form=form)
