9532d99273d7a5a2299e875c798e30f5b4e3d9db
glenchiridza <glenchiridz@gmail.com>
2023-03-31 15:03:45 +0200

    Registration done

--- market/__init__.py
+++ market/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
+from flask_bcrypt import Bcrypt
+
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///market.db"
 app.config['SECRET_KEY'] = 'e7ca173068401e2f66dd47f0'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from market import routes
--- market/models.py
+++ market/models.py
@@ -1,13 +1,21 @@
-from market import db
+from market import db, bcrypt
 
 
 class User(db.Model):
-    id = db.Column(db.Integer(),primary_key=True)
-    username = db.Column(db.String(length=30),nullable=False,unique=True)
-    email = db.Column(db.String(length=50),nullable=False,unique=True)
-    password_hash = db.Column(db.String(length=60),nullable=False)
-    budget = db.Column(db.Integer(),default=1000)
-    items = db.relationship('Item',backref='owning_user',lazy=True)
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+    budget = db.Column(db.Integer(), default=1000)
+    items = db.relationship('Item', backref='owning_user', lazy=True)
+
+    @property
+    def password(self):
+        return self.password
+
+    @password.setter
+    def password(self, plain_text_pass):
+        self.password_hash = bcrypt.generate_password_hash(plain_text_pass).decode('utf-8')
 
     def __repr__(self):
         return self.username
@@ -19,10 +27,9 @@ class Item(db.Model):
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=30), nullable=False, unique=True)
     description = db.Column(db.String(length=1024), nullable=False, unique=True)
-    owner = db.Column(db.Integer(),db.ForeignKey('user.id'))
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
 
     def __repr__(self):
         return self.name
-
 
 # ended at 2:10
